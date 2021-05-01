bc612f40b7589604777ba264df65b7febfe8faab
enriiquealvarez <74270914+enriiquealvarez@users.noreply.github.com>
2021-05-01 10:52:38 -0500

    Update nominas.php

--- nominas.php
+++ nominas.php
@@ -82,7 +82,7 @@
     <div id="wrapper">
 
         <!-- Sidebar -->
-        <ul class="navbar-nav background-color:aliceblue; sidebar sidebar-dark accordion" id="accordionSidebar">
+        <ul class="navbar-nav bg-gradient-secondary background-color:aliceblue; sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
